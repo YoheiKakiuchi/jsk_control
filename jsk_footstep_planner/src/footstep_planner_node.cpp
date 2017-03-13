44ab840f829a9c55a8a252422b1ae885478b954d
YoheiKakiuchi <youhei@jsk.imi.i.u-tokyo.ac.jp>
2017-03-13 15:57:24 +0900

    HAVE TO REMOVE before PR

--- jsk_footstep_planner/src/footstep_planner_node.cpp
+++ jsk_footstep_planner/src/footstep_planner_node.cpp
@@ -34,7 +34,10 @@
  *********************************************************************/
 
 #include "jsk_footstep_planner/footstep_planner.h"
-
+#include "jsk_footstep_planner/footstep_graph.h"
+////
+#include <visualization_msgs/MarkerArray.h>
+////
 #define DEBUG 0
 #if DEBUG
 #include <visualization_msgs/MarkerArray.h>
@@ -52,5 +55,98 @@ int main(int argc, char** argv)
   jsk_footstep_planner::pub_debug_marker = pnh.advertise<visualization_msgs::MarkerArray>("debug_marker_array", 1);
 #endif
   jsk_footstep_planner::FootstepPlanner planner(pnh);
+  /////
+  ros::Publisher pub_line_debug = pnh.advertise<visualization_msgs::MarkerArray>("debug_marker_array", 1);
+  ros::Duration sl(2.0);
+  sl.sleep();
+#if 0
+  Eigen::Vector3f p0(0, 0, 0);
+  Eigen::Vector3f p1(-1, 3, 0);
+  Eigen::Vector3f p2(3, 4, 0);
+  Eigen::Vector3f p3(4, 4, 0);
+  std::vector <Eigen::Vector3f> pts;
+  pts.push_back(p0);
+  pts.push_back(p1);
+  pts.push_back(p2);
+  pts.push_back(p3);
+#endif
+  Eigen::Vector3f p0(0, 0, 0);
+  Eigen::Vector3f p1(0, -1, 0);
+  Eigen::Vector3f p2(0, -2, 0);
+  Eigen::Vector3f p3(0, -4, 0);
+  std::vector <Eigen::Vector3f> pts;
+  pts.push_back(p0);
+  pts.push_back(p1);
+  pts.push_back(p2);
+  pts.push_back(p3);
+  jsk_recognition_utils::PolyLine la(pts);
+  ROS_INFO("length = %f", la.length());
+
+  planner.setHeuristicPathLine(la);
+
+  visualization_msgs::Marker mk;
+  mk.color.r = 1;
+  mk.color.g = 0;
+  mk.color.b = 0;
+  la.toMarker(mk);
+  mk.header.frame_id = "odom";
+  mk.id = 0;
+  Eigen::Vector3f q(3, 2, 0);
+  Eigen::Vector3f ft;
+  double distance_to_goal, alp;
+  int idx;
+  double dist = la.distanceToPoint(q, ft, distance_to_goal, idx, alp);
+
+  la.print();
+
+  ROS_INFO("dist = %f, foot = [%f %f %f] (%d on %f), to_goal = %f\n",
+           dist, ft[0], ft[1], ft[2], idx, alp,
+           distance_to_goal);
+
+  visualization_msgs::MarkerArray ma;
+  ma.markers.push_back(mk);
+  {
+    visualization_msgs::Marker pm;
+    pm.type = visualization_msgs::Marker::POINTS;
+    pm.id = 2;
+    pm.header.frame_id = "odom";
+    geometry_msgs::Point pp;
+    pp.x = q[0];
+    pp.y = q[1];
+    pp.z = q[2];
+    pm.points.push_back(pp);
+    pm.scale.x = 0.04;
+    pm.scale.y = 0.04;
+    pm.color.a = 1;
+    pm.color.r = 0;
+    pm.color.g = 0;
+    pm.color.b = 1;
+    ma.markers.push_back(pm);
+  }
+  {
+    visualization_msgs::Marker pm;
+    pm.type = visualization_msgs::Marker::ARROW;
+    pm.id = 3;
+    pm.header.frame_id = "odom";
+    geometry_msgs::Point st;
+    geometry_msgs::Point ed;
+    st.x = q[0];
+    st.y = q[1];
+    st.z = q[2];
+    ed.x = ft[0];
+    ed.y = ft[1];
+    ed.z = ft[2];
+    pm.points.push_back(st);
+    pm.points.push_back(ed);
+    pm.scale.x = 0.015;
+    pm.scale.y = 0.025;
+    pm.color.a = 1;
+    pm.color.r = 0;
+    pm.color.g = 1;
+    pm.color.b = 0;
+    ma.markers.push_back(pm);
+  }
+  pub_line_debug.publish(ma);
+  /////
   ros::spin();
 }
